74c25435f6585e7d2b3f0b57f7f66652ff14d829
Malik Fajar Mubarok <mubarok.malikfajar@gmail.com>
2025-01-14 13:49:51 +0700

    Remove unused func and add delay before drawing path

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import math
-import random
-import numpy as np
-import tkinter as tk
 import matplotlib.pyplot as plt
+import numpy as np
+import random
+import time
+import tkinter as tk
 
 # Define the environment and GA parameters
 # tkinter canvas coordinate starts from top-left
@@ -11,13 +12,16 @@ goal_point = (10.0, 1.0)
 chromosome_length = 10
 population_size = 40
 elitism_rate = 0.05
-mutation_probability = 0.5
-mutation_range = 1.0
+# mutation_probability = 0.3  # initial
+mutation_probability = 0.5  # best
+# mutation_range = 0.5  # initial
+mutation_range = 0.75  # best
+num_iterations = 1000
 
-# obstacles = [(2.5, 3.0), (4.5, 4.0), (3.0, 5.0)]  # Example obstacles
 obstacles = []
 obstacles_type = []
-for i in range(random.randint(5, 10)):
+# for i in range(random.randint(5, 10)):
+for i in range(random.randint(3, 7)):
     obstacle = (float(random.randint(2, 9)), float(random.randint(2, 9)))
 
     # Prevent creating obstacle in the same coordinate.
@@ -27,6 +31,25 @@ for i in range(random.randint(5, 10)):
     obstacles.append(obstacle)
     obstacles_type.append(random.choice([1, 2, 3]))
 
+# 4 obstacles
+# obstacles = [(8.0, 8.0), (5.0, 4.0), (3.0, 2.0), (9.0, 7.0)]
+# obstacles_type = [1, 1, 2, 3]
+
+# 5 obstacles
+# obstacles = [(3.0, 8.0), (8.0, 7.0), (3.0, 7.0), (9.0, 9.0), (2.0, 6.0)]
+# obstacles_type = [1, 1, 2, 3, 3]
+
+# 6 obstacles
+# obstacles = [(5.0, 9.0), (5.0, 8.0), (5.0, 7.0), (9.0, 5.0), (3.0, 6.0), (8.0, 3.0)]
+# obstacles_type = [1, 3, 3, 1, 1, 3]
+
+# 7 obstacles
+# obstacles = [(3.0, 2.0), (8.0, 6.0), (6.0, 8.0), (4.0, 4.0), (9.0, 3.0), (4.0, 5.0)]
+# obstacles_type = [3, 3, 2, 3, 1, 2]
+
+print(obstacles)  # Print obstacles coordinates
+print(obstacles_type)  # Print obstacles types
+
 # Helper functions for Genetic Algorithm
 def initialize_population():
     population = []
@@ -35,13 +58,8 @@ def initialize_population():
         for _ in range(chromosome_length - 2):
             chromosome.append((random.uniform(0, 10), random.uniform(0, 10)))
         chromosome.append(goal_point)  # Goal point
-        population.append(pad_chromosome(chromosome))
+        population.append(chromosome)
     return population
-
-def pad_chromosome(chromosome):
-    while len(chromosome) < chromosome_length:
-        chromosome.insert(-1, chromosome[-2])
-    return chromosome
 
 def fitness(chromosome):
     total_cost = 0
@@ -58,8 +76,10 @@ def fitness(chromosome):
             collisions += 1
     
     min_distance_to_obstacle = min([distance_to_obstacle(chromosome) for obstacle in obstacles])
-    total_cost = (collisions * 1000) + min_distance_to_obstacle + smoothness + path_distance
-    return 1 / total_cost
+    # total_cost = (collisions * 1000) + (min_distance_to_obstacle * 100) + (smoothness * 10) + path_distance  # initial
+    total_cost = (collisions * 1000) + min_distance_to_obstacle + smoothness + path_distance  # best
+    
+    return (1 / total_cost, path_distance)
 
 def point_line_distance(point, line_start, line_end):
     """Calculate the perpendicular distance from a point to a line segment."""
@@ -116,7 +136,7 @@ def crossover(parent1, parent2):
         new_y = alpha * parent1[i][1] + (1 - alpha) * parent2[i][1]
         child.append((new_x, new_y))
     child.append(goal_point)
-    return pad_chromosome(child)
+    return child
 
 def mutate(chromosome):
     for i in range(1, chromosome_length - 1):
@@ -132,7 +152,7 @@ def elitism(population):
 
 # Visualize the Genetic Algorithm process with tkinter
 class GeneticPathfinderUI:
-    def __init__(self, generations=100):
+    def __init__(self, generations):
         self.max_generations = generations
         self.generations = generations
         self.population = initialize_population()
@@ -144,21 +164,28 @@ class GeneticPathfinderUI:
         self.best_fitness = 0
         self.best_fitness_each_gen = []
         self.best_solution = []
-        self.update_generation()
+        self.best_path_distance = 0
+        self.show_map()  # Show the initial map with obstacles
+        self.root.after(10000, self.update_generation)  # Pause for 10 seconds before updating
+
+    def show_map(self):
+        self.draw_path()
+        print("The simulation will be started in 10 seconds...")
 
     def update_generation(self):
         self.population = rank_selection(self.population)
         best_path = self.population[0]
-        best_fitness = fitness(best_path)
+        best_fitness, best_path_distance = fitness(best_path)
         
         # Logging best fitness for each generation
-        print(f"Generation {self.max_generations - self.generations + 1}: Best fitness = {best_fitness}")
+        print(f"Generation {self.max_generations - self.generations + 1}: Best fitness = {best_fitness} | Best path distance = {best_path_distance}")
 
         # Store the best solution and its fitness
         if best_fitness > self.best_fitness:
             self.best_fitness = best_fitness
             self.best_solution = best_path
-
+            self.best_path_distance = best_path_distance
+        
         self.draw_path(best_path)
         self.best_fitness_each_gen.append(self.best_fitness)
         
@@ -171,8 +198,12 @@ class GeneticPathfinderUI:
         self.population = new_population
 
         self.generations -= 1
-        if self.generations > 0:
-            self.root.after(100, self.update_generation)
+        if self.best_fitness < 0.053:
+            if self.generations > 0:
+                self.root.after(100, self.update_generation)
+            else:
+                print("Doesn't reach convergence, best solution so far:", self.best_solution)
+                self.plot_fitness()
         else:
             # Log the final best solution at the end
             print("Best solution found:", self.best_solution)
@@ -188,16 +219,9 @@ class GeneticPathfinderUI:
         plt.legend()
         plt.show()
 
-    def draw_path(self, path):
+    def draw_path(self, path = []):
         self.canvas.delete("all")
         self.draw_obstacles()
-        
-        for i in range(len(path) - 1):
-            x1, y1 = path[i]
-            x2, y2 = path[i + 1]
-            self.canvas.create_line(
-                x1 * 70, y1 * 70, x2 * 70, y2 * 70, fill="blue", width=2
-            )
 
         self.canvas.create_oval(
             start_point[0] * 70 - 5, start_point[1] * 70 - 5,
@@ -210,12 +234,17 @@ class GeneticPathfinderUI:
             fill="red"
         )
 
+        if len(path) > 0:
+            for i in range(len(path) - 1):
+                x1, y1 = path[i]
+                x2, y2 = path[i + 1]
+                self.canvas.create_line(
+                    x1 * 70, y1 * 70, x2 * 70, y2 * 70, fill="blue", width=2
+                )
+
     def draw_obstacles(self):
         for idx, obs in enumerate(obstacles):
             x, y = obs
-
-            # 1 = rectangle, 2 = triangle, 3 = oval
-            obs_type = random.choice([1, 2, 3])
 
             if obstacles_type[idx] == 1:
                 self.canvas.create_rectangle(
@@ -243,5 +272,5 @@ class GeneticPathfinderUI:
                 )
 
 # Run the UI application
-ui = GeneticPathfinderUI(generations=200)
+ui = GeneticPathfinderUI(generations=num_iterations)
 ui.root.mainloop()
